d27d48ac73ed299afedc15b0c0626b479e8f6995
Tiago Urbano <tiago.urbano@gmail.com>
2021-07-25 11:52:24 -0300

    Adds unit test and component test for app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,54 @@
-import { TestBed, async } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  describe('Unit Test', () => {
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule
+        ],
+        declarations: [
+          AppComponent
+        ],
+        schemas: [NO_ERRORS_SCHEMA],
+      }).compileComponents();
+    }));
+
+    it('should create the app', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app).toBeTruthy();
+    });
   });
 
-  it(`should have as title 'timely'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('timely');
-  });
+  describe('Component test', () => {
+    let fixture: ComponentFixture<AppComponent>;
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('timely app is running!');
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          MatToolbarModule,
+        ],
+        declarations: [AppComponent],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      }).compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+    });
+
+    it('should render mat-toolbar', () => {
+      const element = fixture.debugElement.query(By.css('#app-toolbar')).nativeElement;
+      expect(element).toBeTruthy();
+    });
   });
 });
