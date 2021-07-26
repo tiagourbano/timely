572546a2ab6a98f7c9163fd5135fa87977a259bf
Tiago Urbano <tiago.urbano@gmail.com>
2021-07-25 22:22:38 -0300

    Adds header filters component to home page

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,5 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { HomeComponent } from './home.component';
 
@@ -23,5 +24,13 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain header and list of events', () => {
+    const headerFiltersComponent = fixture.debugElement.query(By.css('app-header-filters'));
+    const listEventsComponent = fixture.debugElement.query(By.css('app-list-events'));
+
+    expect(headerFiltersComponent).toBeTruthy();
+    expect(listEventsComponent).toBeTruthy();
   });
 });
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,24 +2,42 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
-import { MatCardModule } from '@angular/material/card';
+import {
+  MatCardModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { HomeComponent } from './home.component';
 import { ListEventsComponent } from './list-events/list-events.component';
 import { SafeHtmlPipe } from './../../pipes/safe-html.pipe';
+import { HeaderFiltersComponent } from './header-filters/header-filters.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     ListEventsComponent,
-    SafeHtmlPipe
+    SafeHtmlPipe,
+    HeaderFiltersComponent
   ],
   imports: [
     CommonModule,
     HttpClientModule,
     MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
     FlexLayoutModule
+  ],
+  providers: [
+    MatDatepickerModule
   ]
 })
 export class HomeModule { }
